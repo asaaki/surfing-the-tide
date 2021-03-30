11c1afc77df584408376c2d4c6d2a17d83ef0f44
Christoph Grabo <asaaki@mannaz.cc>
2021-03-30 18:47:02 +0200

    Use git repos for in-development versions

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,62 @@
-use opentelemetry::sdk::propagation::{
-    BaggagePropagator, TextMapCompositePropagator, TraceContextPropagator,
-};
 use opentelemetry::{
-    global,
     trace::{FutureExt, TraceContextExt},
-    Context, KeyValue,
+    Context,
 };
-use opentelemetry_jaeger::Propagator as JaegerPropagator;
-use opentelemetry_semantic_conventions::resource;
 use tide::Request;
+
+mod shared;
 
 const SVC_NAME: &str = env!("CARGO_CRATE_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-include!(concat!(env!("OUT_DIR"), "/build_vars.rs"));
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tide::log::start();
-    init_global_propagator();
-
-    let tags = [
-        resource::SERVICE_VERSION.string(VERSION),
-        resource::SERVICE_INSTANCE_ID.string("surf-the-tide"),
-        resource::PROCESS_EXECUTABLE_PATH
-            .string(std::env::current_exe().unwrap().display().to_string()),
-        resource::PROCESS_PID.string(std::process::id().to_string()),
-        KeyValue::new("process.executable.profile", PROFILE),
-    ];
-
-    let (tracer, uninstall) = opentelemetry_jaeger::new_pipeline()
-        .with_service_name(SVC_NAME)
-        .with_tags(tags.iter().map(ToOwned::to_owned))
-        .install()
-        .expect("pipeline install failure");
+    tide::log::with_level(tide::log::LevelFilter::Warn);
+    shared::init_global_propagator();
+    let tracer = shared::jaeger_tracer(SVC_NAME, VERSION, "surf-the-tide-9")?;
 
     let otel_mw = opentelemetry_surf::OpenTelemetryTracingMiddleware::new(tracer.clone());
-    let client = surf::client().with(otel_mw);
+    let client = create_client().with(otel_mw);
 
     let mut app = tide::with_state(client);
 
     app.with(opentelemetry_tide::OpenTelemetryTracingMiddleware::new(
         tracer.clone(),
-        uninstall,
     ));
 
     app.at("/").get(|req: Request<surf::Client>| async move {
         // collect current tracing data, so we can pass it down
         let cx = Context::current();
+        let span = cx.span();
 
         let client = req.state();
-        let uri = "https://httpbin.org/headers";
+        let uri = "http://localhost:3000/";
         let surf_request = client.get(uri).build();
 
+        span.add_event("upstream.request.started".into(), vec![]);
         let mut upstream_res = client.send(surf_request).with_context(cx.clone()).await?;
-        let parsed_res = upstream_res.take_body().into_string().await?;
-        let body = format!("upstream responded with:\n\n{}", parsed_res);
+        let upstream_body = upstream_res.take_body().into_string().await?;
+        let body = format!("upstream responded with: \n{}", upstream_body);
+        span.add_event("upstream.request.finished".into(), vec![]);
         Ok(body)
     });
-    app.listen("127.0.0.1:3000").await?;
 
+    eprintln!("Don't forget to start a service on port 3000.");
+    app.listen("0.0.0.0:4000").await?;
+    opentelemetry::global::shutdown_tracer_provider();
     Ok(())
 }
 
-pub fn init_global_propagator() {
-    global::set_text_map_propagator(composite_propagator());
-    // global::set_text_map_propagator(JaegerPropagator::new());
-}
+// more custom http client setup: use isahc with metrics enabled
+fn create_client() -> surf::Client {
+    use http_client::isahc::IsahcClient;
+    use isahc::prelude::*;
 
-fn composite_propagator() -> TextMapCompositePropagator {
-    let baggage_propagator = BaggagePropagator::new();
-    let jaeger_propagator = JaegerPropagator::new(); // aka Uber headers
-    let trace_context_propagator = TraceContextPropagator::new();
-
-    TextMapCompositePropagator::new(vec![
-        Box::new(jaeger_propagator),
-        Box::new(baggage_propagator),
-        Box::new(trace_context_propagator),
-    ])
+    let isahc = HttpClient::builder()
+        .default_headers(&[("user-agent", "surf/isahc (with request metrics)")])
+        .metrics(true)
+        .build()
+        .expect("isahc client could no be created");
+    let http_client = IsahcClient::from_client(isahc);
+    surf::Client::with_http_client(http_client)
 }
---
+++ src/shared.rs
@@ -0,0 +1,64 @@
+#![doc(hidden)]
+
+use opentelemetry::sdk::{
+    propagation::{BaggagePropagator, TextMapCompositePropagator, TraceContextPropagator},
+    trace::{self, Config, Tracer},
+};
+use opentelemetry::{global, trace::TraceError, KeyValue};
+use opentelemetry_jaeger::Propagator as JaegerPropagator;
+use opentelemetry_semantic_conventions::resource;
+
+include!(concat!(env!("OUT_DIR"), "/build_vars.rs"));
+
+pub fn init_global_propagator() {
+    global::set_text_map_propagator(composite_propagator());
+    // OR you could use a single propagator only:
+    // global::set_text_map_propagator(TraceContextPropagator::new());
+}
+
+fn composite_propagator() -> TextMapCompositePropagator {
+    // W3C spec: https://w3c.github.io/baggage/ - very flexible KV format, can carry more than just trace context data
+    let baggage_propagator = BaggagePropagator::new();
+
+    // Uber's original format - probably only useful in a closed jaeger only setup
+    let jaeger_propagator = JaegerPropagator::new(); // aka Uber headers
+
+    // Yet another W3C spec: https://www.w3.org/TR/trace-context/ - only for trace context info
+    let trace_context_propagator = TraceContextPropagator::new();
+
+    // NB! last wins (and overwrites!); so re-order based on your actual usage or preferences
+    // or leave out propagators you definitely do no use;
+    // of course, if you send all headers with identical values the order doesn't matter.
+    TextMapCompositePropagator::new(vec![
+        Box::new(jaeger_propagator),
+        Box::new(baggage_propagator),
+        Box::new(trace_context_propagator),
+    ])
+}
+
+#[allow(dead_code)]
+pub fn trace_config() -> Config {
+    trace::config()
+    // can accept more options like:
+    // .with_sampler(Sampler::TraceIdRatioBased(0.2))
+}
+
+pub fn jaeger_tracer(
+    svc_name: &str,
+    version: &str,
+    instance_id: &str,
+) -> Result<Tracer, TraceError> {
+    let tags = [
+        resource::SERVICE_VERSION.string(version.to_owned()),
+        resource::SERVICE_INSTANCE_ID.string(instance_id.to_owned()),
+        resource::PROCESS_EXECUTABLE_PATH
+            .string(std::env::current_exe().unwrap().display().to_string()),
+        resource::PROCESS_PID.string(std::process::id().to_string()),
+        KeyValue::new("process.executable.profile", PROFILE),
+    ];
+
+    opentelemetry_jaeger::new_pipeline()
+        .with_service_name(svc_name)
+        .with_tags(tags.iter().map(ToOwned::to_owned))
+        .install_batch(opentelemetry::runtime::AsyncStd)
+}
